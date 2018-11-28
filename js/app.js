cabb6e1e55c536ea894da0ba04c5da3f6dcb24b2
Sylvia L <sylvia@Sylvias-MacBook-Pro.local>
2018-11-27 21:25:12 -1000

    added show and hide menu event

--- js/app.js
+++ js/app.js
@@ -2,7 +2,18 @@
 //Add event listeners to the following:
 
 /*1) to the navmenu items (Home, Location, Blog) that will show and hide the contents for that specific navmenu item.*/
-
+var divElem = document.getElementsByClassName('navi');
+for(var i = 0; i<divElem.length; i++){
+    divElem[i].addEventListener('click', showInner);
+}
+function showInner(){
+    var getInner = this.querySelectorAll('.inner')[0];
+    if(getInner.style.display === 'block'){
+        getInner.style.display = 'none';
+    }else{
+        getInner.style.display = 'block'
+    }
+}
 
 /*2) to each restaurant name that will show and hide (toggle) the secret menu item for that particular restaurant.*/
 
